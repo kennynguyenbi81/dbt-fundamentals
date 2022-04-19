cc171c1a18fd636c2d621b7fd0043afe945c717a
Kenny, Nguyen <T829846@spark.co.nz>
2022-04-19 03:29:20 +0000

    after practice with jinja and macros

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,8 @@ select
     status,
 
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+    {{ cents_to_dollars('amount', 4) }} as amount,
     created as created_at
 
 from {{ source('stripe', 'payment') }}
+{{ limit_dev_data('created_at',1000) }}
---
+++ models/union_tables_by_order_prefix.sql
@@ -0,0 +1,7 @@
+{{
+    union_relations_by_prefix(
+        database='raw',
+        schema='dbt_learn_by_jinja',
+        prefix='orders__'
+    )
+}}
